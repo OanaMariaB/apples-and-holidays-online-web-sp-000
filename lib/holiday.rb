81889ec8b36c967842723bb083b52e2b24c0ec37
Oana Berentan <oana.berentan@yahoo.com>
2019-05-14 22:12:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,7 +47,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
     holiday_hash.each do |season, holiday|
       holiday.each do |holiday, item|
-        if item.include("BBQ")
+        if item.include?("BBQ")
           return item
         end
         end
